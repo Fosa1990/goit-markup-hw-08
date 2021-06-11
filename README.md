ea350392f0909392217849bdbc7727b6b802b431
Fosa1990 <fosa1990@gmail.com>
2021-06-11 10:00:07 +0300

    copy from 07 to 08

---
+++ README.md
@@ -0,0 +1,5 @@
+https://fosa1990.github.io/goit-markup-hw-08/
+
+# goit-markup-hw-08
+
+home work 08 tech skills
---
+++ js/modal.js
@@ -0,0 +1,19 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+
+// data-modal-open - на кнопку открытия модального окна.
+// data-modal-close - на кнопку закрытия модального окна.
+// data-modal - на бекдроп модального окна.
